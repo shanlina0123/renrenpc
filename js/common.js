c29aca3c6964228a2cae2802f172987e362f8928
zhaoying111 <2864342582@qq.com>
2018-04-19 14:47:40 +0800

    公共引用修改

--- js/common.js
+++ js/common.js
@@ -1,21 +1,26 @@
- // 顶部导航右侧操作
- $("body").on("click", ".layui-nav .layui-nav-item a", function() {
-     $(this).siblings().addClass("layui-show");
- });
- //左侧导航效果
- $(".leftNav > li > a").click(function() {
-     if ($(this).hasClass("on")) {
-         $(this).siblings(".leftSubNav").slideDown();
-         $(this).parents("li").siblings().find(".leftSubNav").slideUp();
-         $(this).addClass("on").parents("li").siblings().find("a").removeClass("on");
-     } else {
-         $(this).removeClass("on");
-         $(this).siblings(".leftSubNav").slideUp();
-     }
- });
- //导航添加背景图片
- $(".leftNav > li >a").each(function() {
-     if ($(this).siblings().hasClass("leftSubNav")) {
-         $(this).addClass("hasBg")
-     }
- });
+$(function() {
+    //添加公用页面
+    $("#top").load('../public/top.html');
+    $("#left").load('../public/left.html');
+    //左侧导航效果
+    $(".leftNav>li>a").click(function() {
+        if (!$(this).hasClass("on")) {
+            $(this).siblings(".leftSubNav").slideDown();
+            $(this).parents("li").siblings().find(".leftSubNav").slideUp();
+            $(this).addClass("on").parents("li").siblings().find("a").removeClass("on");
+        } else {
+            $(this).removeClass("on");
+            $(this).siblings(".leftSubNav").slideUp();
+        }
+    });
+    //导航添加背景图片
+    $(".leftNav > li >a").each(function() {
+        if ($(this).siblings().hasClass("leftSubNav")) {
+            $(this).addClass("hasBg")
+        }
+    });
+    // 顶部导航右侧操作
+    $("body").on("click", ".layui-nav .layui-nav-item a", function() {
+        $(this).siblings().addClass("layui-show");
+    });
+})
