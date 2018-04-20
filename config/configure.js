bf501b52f8c0c6c282b56a1399dcc84feec9737d
zhaoying111 <2864342582@qq.com>
2018-04-20 17:32:04 +0800

    用户列表和查看功能

--- config/configure.js
+++ config/configure.js
@@ -1,40 +1,38 @@
-(function () {
+(function() {
     var host = "http://api.rrzhaofang.com/";
     //未带toke请求
     window.conf = {
-        login: host + 'admin/login',//首页推荐
-    },
-    //带token的请求
-    window.auth_conf = {
-        path_url:'http://api.rrzhaofang.com/upload/',//图片地址
-        house_list:host+'admin/house/index', //房源列表
-        house_recommend:host+'admin/house/recommend',//房源推荐
-        house_delete:host+'admin/house/delete/',//房源删除
-        house_form_data:host+'admin/house/create',//添加房源数据
-        house_add:host+'admin/house/store',//添加房源数据
-        datas_default_one: host + 'admin/datas-default-one/',//默认属性数据
-        datas_one: host + 'admin/datas-one/',//自定义数据单个
+            login: host + 'admin/login', //首页推荐
+        },
+        //带token的请求
+        window.auth_conf = {
+            path_url: 'http://api.rrzhaofang.com/upload/', //图片地址
+            house_list: host + 'admin/house/index', //房源列表
+            house_recommend: host + 'admin/house/recommend', //房源推荐
+            house_delete: host + 'admin/house/delete/', //房源删除
+            house_form_data: host + 'admin/house/create', //添加房源数据
+            house_add: host + 'admin/house/store', //添加房源数据
+            datas_default_one: host + 'admin/datas-default-one/', //默认属性数据
+            datas_one: host + 'admin/datas-one/', //自定义数据单个
+            admin_list: host + 'admin/admin', //用户列表
+            admin_edit: host + 'admin/admin/' //用户编辑
 
-
-    }
+        }
 
     /**
      * 检查token 过期时间
      */
     var userinfo = localStorage.getItem("userinfo");
-    if( userinfo == 'undefined' )
-    {
-        window.location.href='/login.html';
+    if (userinfo == 'undefined') {
+        window.location.href = '/login.html';
 
-    }else
-    {
+    } else {
         var timestamp = Date.parse(new Date());
         timestamp = timestamp / 1000;
         userinfo = JSON.parse(userinfo);
-        if( timestamp > userinfo.expiration )
-        {
+        if (timestamp > userinfo.expiration) {
             localStorage.removeItem("userinfo");
-            window.location.href='/login.html';
+            window.location.href = '/login.html';
         }
     }
     /**
@@ -42,4 +40,4 @@
      */
     $("#top").load('/page/public/top.html');
     $("#left").load('/page/public/left.html');
-})();
+}());
---
+++ js/page/admin/admin.js
@@ -0,0 +1,99 @@
+$(function() {
+    //这个页面的分页用的是浏览器解析后的布局（其他页面使用的是解析前的，后期如果需要会统一修改）
+    //          layui.use(['laypage', 'layer', 'form'], function() {
+    //              var laypage = layui.laypage,
+    //                  layForm = layui.layform,
+    //                  layer = layui.layer;
+    //              //总页数大于页码总数
+    //              laypage.render({
+    //                  elem: 'pagerInner',
+    //                  count: 70, //数据总数
+    //                  jump: function(obj) {
+    //                      //console.log(obj)
+    //                  }
+    //              });
+    //          });
+    //注册layer
+    // layui.use('form', function() {
+    //     var form = layui.form;
+    // })
+
+});
+//验证
+if ($(".layui-form2").length) {
+    $(".layui-form2").Validform({
+        btnSubmit: '.layui-btn',
+        tiptype: 1,
+        postonce: true,
+        showAllError: false,
+        tiptype: function(msg, o, cssctl) {
+            if (!o.obj.is("form")) {
+                if (o.type != 2) {
+                    var objtip = o.obj.parents('.layui-form-item').find(".Validform_checktip");
+                    objtip.addClass('Validform_skate');
+                    cssctl(objtip, o.type);
+                    layer.msg(msg, { icon: 5, time: 2000, shift: 6 });
+                }
+            }
+        }
+    });
+}
+var vm = new Vue({
+    el: '#adminTable',
+    data: {
+        tokenValue: JSON.parse(localStorage.getItem("userinfo")).token, //token
+        adminList: [], //用户列表
+        adminEdit: '', //编辑用户nickname是姓名，name是账号
+
+    },
+    methods: {
+        //获取用户列表
+        getAdminList: function() {
+            var that = this;
+            var url = auth_conf.admin_list;
+            axios.get(url, { headers: { "Authorization": that.tokenValue } }).then(function(response) {
+                var data = response.data;
+                if (data.status == 1) {
+                    that.adminList = data.data;
+                    console.log(data);
+                    layui.use(['form'], function() {
+                        var form = layui.form;
+                        form.render('checkbox');
+                    });
+                } else {
+
+                }
+            })
+        },
+        //编辑用户
+        getEditAdmin: function(uuid) {
+            console.log(uuid);
+            var that = this;
+            var url = auth_conf.admin_edit + uuid;
+            axios.get(url, { headers: { "Authorization": that.tokenValue } }).then(function(response) {
+                var data = response.data;
+                if (data.status == 1) {
+                    that.adminEdit = data.data;
+                    layer.open({
+                        type: 1,
+                        title: '新增编辑用户',
+                        shadeClose: true,
+                        scrollbar: false,
+                        skin: 'layui-layer-rim',
+                        area: ['500px', '500px'],
+                        content: $(".userPopWrap")
+                    })
+                } else if (data.status == 10) {
+                    layer.msg("抱歉，您不能查看管理员信息！");
+                }
+            })
+        },
+        //删除用户
+
+    },
+    created: function() {
+        var that = this;
+        that.getAdminList(); //用户列表
+        that.getEditAdmin(); //编辑用户
+    }
+})
