e1362e377df56db7e2a92b1ee8083be23ddd7862
单莉娜 <1161110682@qq.com>
2018-04-23 13:51:25 +0800

    跟进

--- config/configure.js
+++ config/configure.js
@@ -1,5 +1,5 @@
 (function() {
-    var host = "http://192.168.15.222:8081/";
+    var host = "http://9=192.168.15.222:8081";
     //未带toke请求
     window.conf = {
         login: host + 'admin/login', //首页推荐
@@ -18,7 +18,7 @@
             add_imag: host + 'admin/img/upload', //添加房源数据
             datas_default_one: host + 'admin/datas-default-one/', //默认属性数据
             datas_one: host + 'admin/datas-one/', //自定义数据单个
-            
+
             admin_datas: host + 'admin/chart-admin',//后台用户列表
             users_list: host + 'admin/user/broker', //经纪人列表
             chart_list: host + 'admin/chart', //数据列表
@@ -29,66 +29,65 @@
             admin_edit: host + 'admin/admin/', //编辑查看用户
             admin_lock: host + 'admin/admin-setting/', //锁定用户
             role_list: host + 'admin/roles', //角色列表
-            client_follow_list:host + 'admin/client-follow/edit/' //客户跟进列表
+            client_follow_list:host + 'admin/client-follow/edit/', //客户跟进列表
+            client_follow:host + 'admin/client-follow/store' //跟进客户
         }
-
+    $("#top").load('/page/public/top.html');
+   $("#left").load('/page/public/left.html');
 })();
-
-/*new Vue({
-    el: '.wrap',
-    data: {
-        tokenData: localStorage.getItem("userinfo")
-    },
-    methods: {
-        filterToken: function() {
-            var that = this;
-            if (!that.tokenData) {
-                window.location = "/login.html";
-            } else {
-                if (!JSON.parse(that.tokenData).token) {
-                    window.location = "/login.html";
-                }
-                //检查token是否失效
-                that.checkToken();
-            }
-        },
-        checkToken: function() {
-            var url = auth_conf.token;
-            var that = this;
-            axios.get(url, { headers: { "Authorization": JSON.parse(that.tokenData).token } })
-                .then(function(response) {
-                    var data = response.data;
-                    if (data.status != 1) {
-                        if (data.messages) {
-                            //alert(data.messages);
-                            // layui.use('layer', function(id) {
-                            //     var layer = layui.layer;
-                            //     layer.msg(data.messages);
-                            // });
-                        }
-                        localStorage.removeItem("userinfo");
-                        that.tokenData = null;
-                        window.location = "/login.html";
-                    }
-                    /!**
-                     * 引入top和right
-                     *!/
-                    $("#top").load('/page/public/top.html');
-                    $("#left").load('/page/public/left.html');
-
-                })
-                .catch(function (error)
-                {
-                    alert("Token验证异常");
-                    window.location="/login.html";
-                });
-        }
-    }
-    ,created: function () {
-        var that = this;
-        that.filterToken();//过滤token
-    }
-<<<<<<< HEAD
-});*/
-
-
+//
+// new Vue({
+//     el: '.wrap',
+//     data: {
+//         tokenData: localStorage.getItem("userinfo")
+//     },
+//     methods: {
+//         filterToken: function() {
+//             var that = this;
+//             if (!that.tokenData) {
+//                 window.location = "/login.html";
+//             } else {
+//                 if (!JSON.parse(that.tokenData).token) {
+//                     window.location = "/login.html";
+//                 }
+//                 //检查token是否失效
+//                 that.checkToken();
+//             }
+//         },
+//         checkToken: function() {
+//             var url = auth_conf.token;
+//             var that = this;
+//             axios.get(url, { headers: { "Authorization": JSON.parse(that.tokenData).token } })
+//                 .then(function(response) {
+//                     var data = response.data;
+//                     if (data.status != 1) {
+//                         if (data.messages) {
+//                             //alert(data.messages);
+//                             // layui.use('layer', function(id) {
+//                             //     var layer = layui.layer;
+//                             //     layer.msg(data.messages);
+//                             // });
+//                         }
+//                         localStorage.removeItem("userinfo");
+//                         that.tokenData = null;
+//                         window.location = "/login.html";
+//                     }
+//                     $("#top").load('/page/public/top.html');
+//                     $("#left").load('/page/public/left.html');
+//
+//                 })
+//                 .catch(function (error)
+//                 {
+//                     alert("Token验证异常");
+//                     window.location="/login.html";
+//                 });
+//         }
+//     }
+//     ,created: function () {
+//         var that = this;
+//         that.filterToken();//过滤token
+//     }
+//
+// });
+//
+//
